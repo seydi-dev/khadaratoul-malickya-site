104f4d333a18dd40d11345d667d79adb5a0ea81c
Seydi <amethndiaye16641@gmail.com>
2026-08-19 19:28:53 -0400

    quotes

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -2,6 +2,12 @@
 export const QUOTES = [
   {
     transliteration:
+      "« min kulli wasfın hamiidin haaza af'ala tafdiilin rajaa-ul baraaya yawma muzdahami. »",
+    fr: "En toute belle qualité, le Refuge au jour de trouble a obtenu le meilleur.",
+    en: "Of truly fine quality, the Refuge in the day of trouble has secured the best.",
+  },
+  {
+    transliteration:
       "« Amâtanâ Lâhou Alâ Dîni Nabî, Wa Houbbi Shaykhinâl Imâmi Tayyibi. »",
     fr: "Qu'Allah nous donne la mort dans la religion de l'Envoyé ﷺ, ainsi que dans l'amour de notre Shaykh (At-Tidjani), l'Imam, le Pur.",
     en: "May Allah grant us death in the religion of the Messenger ﷺ, and in the love of our Shaykh (At-Tidjani), the Imam, the Pure.",
